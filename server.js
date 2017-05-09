b974550f40fe06434887b04b9bc4bc7bde252e4c
Paul Walsh <twentytwentyk@live.com>
2017-05-09 15:54:48 -0400

    finished part 1:crud methods

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express'); // call express
 var app = express(); // define our app using express
 var bodyParser = require('body-parser');
-var mongoos = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database');
 var Bear = require('./app/models/bear');
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -10,12 +11,68 @@ app.use(bodyParser.urlencoded({ extended: true})); //Parses the text as URL enco
 
 app.use(bodyParser.json()); //Parses the text as JSON and exposes the resulting object on req.body.
 
-//Only after setting the req.body to the desirable contents will it call the next middleware in the stack, which can then access the request data without having to think about how to unzip and parse it.
+//Only after setting the req.body
+ // to the desirable contents will it call the next middleware in the stack, which can then access the request data without having to think about how to unzip and parse it.
 
-var port =process.env.PORT || 8080; //set our PORT
+var port = process.env.PORT || 8080; //set our PORT
 
 var router = express.Router(); // get an instance of the express Router
+//middleware to use for all requests
+router.use(function(req, res, next){
+  console.log('Something is definitely happening.');
+  next();
+});
 
+router.route('/bears')
+  .post(function(req, res){
+      var bear = new Bear();
+      bear.name = req.body.name;// set the bears name (comes from the request)
+      bear.save(function(err){
+        if (err)
+          res.send(err)
+
+        res.json({message: 'Bear Created!'});
+      });
+  })
+  .get(function(req, res){
+    Bear.find(function(err, bears){
+      if (err)
+        res.send(err);
+
+      res.json(bears);
+    });
+  });
+
+router.route('/bears/:bear_id')
+  .get(function(req, res){
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err)
+        res.send(err);
+      res.json(bear);
+    });
+  })
+  .put(function(req, res){
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err)
+        res.send(err);
+      bear.name = req.body.name;
+
+      bear.save(function(err) {
+        if (err)
+          res.send(err);
+        res.json({ message: 'Bear updated!'});
+      });
+    });
+  })
+  .delete(function(req, res){
+    Bear.remove({
+      _id: req.params.bear_id
+    }, function(err, bear){
+      if (err)
+        res.send(err);
+      res.json({ message : "Successfully Deleted!"});
+    });
+  });
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 
 router.get('/', function(req, res) {
